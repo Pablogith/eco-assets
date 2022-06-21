2c05a4f91d7e4fd1b38691956ddcfb14626ca5b4
Pawel Relinski <pawelrelinski@gmail.com>
2022-06-21 19:51:58 +0200

    (feat) Add angular applications

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eac-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
